9306329866a82e841d52c20aed7ce69207dbb0c7
Bryan Yee <yee.bryan.c@gmail.com>
2020-10-13 13:36:15 -0700

    Add more CRUD actions

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-  def get(self):
-    return {"data": "Hello World"}
+videos_put_args = reqparse.RequestParser()
+videos_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+videos_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+videos_put_args.add_argument("likes", type=int, help="Likes on the video is required", required=True)
 
-api.add_resource(HelloWorld, "/helloworld")
+videos = {}
+
+def abort_if_video_id_doesnt_exist(video_id):
+  if video_id not in videos:
+    abort(404, message="Video is is not valid.")
+
+def abort_if_video_exists(video_id):
+  if video_id in videos:
+    abort(409, message="Video already exists with that ID.")
+
+class Video(Resource):
+  def get(self, video_id):
+    abort_if_video_id_doesnt_exist(video_id)
+    return videos[video_id]
+
+  def put(self, video_id):
+    abort_if_video_exists(video_id)
+    args = videos_put_args.parse_args()
+    print(args)
+    videos[video_id] = args
+    return videos[video_id]
+
+  def delete(self, video_id):
+    abort_if_video_id_doesnt_exist(video_id)
+    del videos[video_id]
+    return '', 204
+
+
+api.add_resource(Video, "/videos/<int:video_id>")
 
 if __name__ == "__main__":
   app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,9 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+# response = requests.put(BASE + "videos/1", {"likes": 10, "name": "My video", "views": 2})
+# print(response.json())
+
+
+response = requests.get(BASE + "videos/2")
 print(response.json())
